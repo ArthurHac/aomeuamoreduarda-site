f7431d7fb2b7be347040c9e67e3881c69fb29998
Arthur Henrique <arthurhenrique@MacBook-Air-de-Arthur.local>
2025-03-07 21:58:51 -0300

    commit initial - vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import "./App.css";
-
 import PrincipalPage from "./components/principal-page/principal";
 
 function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html", // Inclui o arquivo HTML
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclui os arquivos JS/TS/JSX/TSX
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
