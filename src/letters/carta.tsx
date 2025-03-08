188b419a18f933326dce91e8abacd208c15e9e90
Arthur Henrique <arthurhenrique@MacBook-Air-de-Arthur.local>
2025-03-07 22:28:09 -0300

    configuracao de commit

--- src/letters/carta.tsx
+++ src/letters/carta.tsx
@@ -2,31 +2,9 @@ import { Letters } from "./letters.interface";
 
 const CARTAS: Letters[] = [
   {
-    texto: "text 1",
-    titulo: "primeiro texto",
-  },
-  {
-    texto: "text 2",
-    titulo: "segundo texto",
-  },
-  {
-    texto: "text 3",
-    titulo: "terceiro texto",
-  },
-  {
-    texto: "Lorem Ipsum is simply dummy text",
-    titulo: "quarto texto",
-  },
-  {
-    texto:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    titulo: "quinto texto",
-  },
-  {
-    texto:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
-    titulo: "sexto texto",
-  },
+    titulo: "Minha primeira carta tech",
+    texto: " Para a minha primeira carta tech escolhi essa data tao especial q é o dia das mulheres, nao sei se vc sabe, mas vc é a mulher da minha vida, esta a todo momento ao meu lado, por mais q algumas vezes contraria com as minhas besteiras, mas sem la. Sindo um amor mto grande pela sua pessoa que vai aumentando com o tempo, acho q é assim q os velhinhos se sentem, quero a todo momento ficar ao seu lado, fazer as coisas com vc e está la para vc, nao sou perfeito, mas estou procurando melhor para ser digno de tal beleza grega. Acho que vamos sim ter o nosso canto e isso vai selar o nosso destino juntos, nem em sonhos eu imaginaria esta com alguem tao divertido e engracado, praticamente um meme abulante, seu jeito é uma das minhas coisas favoridas depois dos seus olhos (claro), mas voltando a linha de raciocio, nao tem um dia que eu nn imagine nos dois juntinhos no nosso cantinho, vivendo de matecouro de limao com hamburguinho e vendo series dividosas, pra mim esse vai ser o conceito de riqueza. Aproveitando a deixa, minha memoria estava a cada dia pior, sinto q nn estou lembrando mais do basico que uma pessoa poderia lembrar, entao venho aq pedi um pouco de paciencia com o  TDAH aq, vc sabe o quanto é importante para mim, entáo eu te peco para que quando vc ver q eu esqueci, me lembre hahahaha. \n Voce é o amor da minha vida, é eu fico bolado quando vc pergunta, pq pra mim é algo obvio e o sentimento bolades é automatico, preciso mesmo demostrar mais meu amor por vc, entáo como meta e incentivo, estou usando agr esse correio, vou passar a deixar messagem aq para vc quando der na telha, ou quando vc passar pelo meu pensamento durante o dia, eu te amo e obrigado por tudo. "
+  }
 ];
 
 export default CARTAS;
